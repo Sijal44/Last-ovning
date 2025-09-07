b4adc63feb9fbba9c12e065b67bb431b9e8fd35c
Sijal44 <sajad.azizi92@yahoo.com>
2025-09-07 21:46:36 +0200

    Add project files.

---
+++ Last ovning/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Last_ovning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
